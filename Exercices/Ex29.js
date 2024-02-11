89a5aa8010bc1e4c8ff46c1b56bd5e3cfd934911
Rafael Campos <rafael.campos.m@hotmail.com>
2024-02-11 03:42:49 -0300

    Resolução ex20

--- Exercices/Ex29.js
+++ Exercices/Ex29.js
@@ -1,1 +1,11 @@
 /*Crie uma função que receba um array de números e retorne a soma de todos os elementos.*/
+
+const num = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+const valorInicial = 0;
+
+const soma = num.reduce(
+  (acumulador, valorAtual) => acumulador + valorAtual,
+  valorInicial
+);
+console.log(soma);
